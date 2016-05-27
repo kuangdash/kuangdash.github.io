64485625ddae435ea0201adc2fb12bdde755fe97
U-DASH2\kuangdash <kuangdash@126.com>
2016-05-27 22:24:03 +0200

    --Committed by EGO

--- themes/haru/resources/js/main.js
+++ themes/haru/resources/js/main.js
@@ -1,19 +1,5 @@
 //(require 'jquery)
 
-/* comments */
-var kd_disqus_thread=$('#disqus_thread');
-var ds_label=$('.ds-thread');
-
-$('.disqus_label').click(function(){
-    kd_disqus_thread.show();
-    ds_label.hide();
-});
-
-$('.ds-label').click(function(){
-    kd_disqus_thread.hide();
-    ds_label.show();
-});
-
 /* navigate */
 window.ego_toc = $('#text-table-of-contents ul li');
 if(0 != window.ego_toc.length){
